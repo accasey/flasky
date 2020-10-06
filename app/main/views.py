374fb4cbebab37ecc06c8c897a6691cf4422e9c1
Andrew Casey <accasey@hotmail.com.au>
2020-10-06 08:37:07 +0100

    comment moderation

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 """The Blueprint's custom routes."""
-from typing import Any
+from typing import Any, Text
 
 from flask import (
     abort,
@@ -317,3 +317,39 @@ def show_followed() -> Response:
     resp = make_response(redirect(url_for(".index")))
     resp.set_cookie("show_followed", "1", max_age=30 * 24 * 60 * 60)  # 30 days
     return resp
+
+
+@main.route("/moderate")
+@login_required
+@permission_required(Permission.MODERATE)
+def moderate() -> Text:
+    page: int = request.args.get("page", 1, type=int)
+    pagination = Comment.query.order_by(Comment.timestamp.desc()).paginate(
+        page, per_page=current_app.config["FLASKY_COMMENTS_PER_PAGE"], error_out=False
+    )
+    comments = pagination.items
+    return render_template(
+        "moderate.html", comments=comments, pagination=pagination, page=page
+    )
+
+
+@main.route("/moderate/enable/<int:id>")
+@login_required
+@permission_required(Permission.MODERATE)
+def moderate_enable(id: int):
+    comment = Comment.query.get_or_404(id)
+    comment.disabled = False
+    db.session.add(comment)
+    db.session.commit()
+    return redirect(url_for(".moderate", page=request.args.get("page", 1, type=int)))
+
+
+@main.route("/moderate/disable/<int:id>")
+@login_required
+@permission_required(Permission.MODERATE)
+def moderate_disable(id: int):
+    comment = Comment.query.get_or_404(id)
+    comment.disabled = True
+    db.session.add(comment)
+    db.session.commit()
+    return redirect(url_for(".moderate", page=request.args.get("page", 1, type=int)))
